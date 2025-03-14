5536e89cbfcffe64177550b1d3df1f759740618a
luiz982 <juninholjl@hotmail.com>
2025-03-14 15:19:16 -0300

    add component

--- src/main/java/com/getinfo/gestaocontratual/config/EnvConfig.java
+++ src/main/java/com/getinfo/gestaocontratual/config/EnvConfig.java
@@ -1,36 +1,19 @@
 package com.getinfo.gestaocontratual.config;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Configuration;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class EnvConfig {
 
-    @PostConstruct
-    public void init() {
-        try {
-            String envFilePath = ".env";
-            System.out.println("📂 Tentando ler o arquivo: " + Paths.get(envFilePath).toAbsolutePath());
+    @Value("${DATABASE_USERNAME}")
+    private String databaseUsername;
 
-            if (!Files.exists(Paths.get(envFilePath))) {
-                System.out.println("❌ ERRO: O arquivo .env não foi encontrado!");
-                return;
-            }
+    @Value("${DATABASE_PASSWORD}")
+    private String databasePassword;
 
-            List<String> lines = Files.readAllLines(Paths.get(envFilePath));
-            if (lines.isEmpty()) {
-                System.out.println("⚠️ Atenção: O arquivo .env está vazio!");
-            }
-
-            System.out.println("✅ Arquivo .env encontrado e lido com sucesso!");
-            lines.forEach(System.out::println);
-
-        } catch (IOException e) {
-            System.out.println("❌ Erro ao ler o .env: " + e.getMessage());
-        }
+    public void printDatabaseConfig() {
+        System.out.println("Database Username: " + databaseUsername);
+        System.out.println("Database Password: " + databasePassword);
     }
 }
