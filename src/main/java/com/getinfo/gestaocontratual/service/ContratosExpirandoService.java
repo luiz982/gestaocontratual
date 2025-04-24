3e4fe03bed7afc3a6ffb263d6427e947324fd596
luiz982 <juninholjl@hotmail.com>
2025-04-24 11:49:28 -0300

    feat: e-mail de aviso para contratos expirando

--- src/main/java/com/getinfo/gestaocontratual/service/ContratosExpirandoService.java
+++ src/main/java/com/getinfo/gestaocontratual/service/ContratosExpirandoService.java
@@ -2,15 +2,21 @@ package com.getinfo.gestaocontratual.service;
 
 import com.getinfo.gestaocontratual.entities.Contrato;
 import com.getinfo.gestaocontratual.repository.ContratoRepository;
-import org.springframework.amqp.rabbit.annotation.*;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.SimpleMailMessage;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
+import java.time.ZoneId;
 
 @Service
 public class ContratosExpirandoService {
@@ -21,26 +27,52 @@ public class ContratosExpirandoService {
     @Autowired
     private ContratoRepository contratoRepository;
 
-    @Scheduled(cron = "0 09 12 * * ?")
-    public void enviarEmailsContratosExpirando() {
-        //LocalDate hoje = LocalDate.now();
-        LocalDate hoje = LocalDate.of(2025, 3, 1);
+    private static final SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+    @Scheduled(cron = "0 27 11 * * ?")
+    public void enviarEmailsContratosExpirando() throws MessagingException {
+        LocalDate hoje = LocalDate.now();
         LocalDate prazo = hoje.plusDays(60);
 
-        List<Contrato> contratosExpirando = contratoRepository.findByDtFimBetween(hoje, prazo);
-
-        for (Contrato contrato : contratosExpirando) {
-            //enviarEmail(contrato.getEmailResponsavel(), contrato);
-            enviarEmail("luiz.jacson@souunit.com.br", contrato);
+        List<Contrato> contratos = contratoRepository.findByDtFimBetween(hoje, prazo);
+        if (contratos.isEmpty()) {
+            return;
         }
-    }
 
-    public void enviarEmail(String destinatario, Contrato contrato) {
-        SimpleMailMessage mensagem = new SimpleMailMessage();
-        mensagem.setTo(destinatario);
-        mensagem.setSubject("Aviso: Seu contrato está prestes a expirar!");
-        mensagem.setText("O contrato de número " + contrato.getNumContrato() +
-                " expira em " + contrato.getDtFim() + ". Por favor, tome as devidas providências.");
-        mailSender.send(mensagem);
+        StringBuilder html = new StringBuilder();
+        String logoUrl = "https://www.getinfo.net.br/static/media/logo-getinfo2.46e29e79.png";
+        html.append("<html><body>");
+        html.append("<div style='text-align:center;margin-bottom:20px;'>")
+                .append("<img src=\"" + logoUrl + "\" alt=\"Getinf\" width=\"210\" height=\"75\">")
+                .append("</div>");
+        html.append("<h3>Contratos prestes a expirar</h3>");
+        html.append("<table border='1' cellpadding='5' cellspacing='0' style='border-collapse:collapse;width:100%'>")
+                .append("<tr>")
+                .append("<th>Nº Contrato</th>")
+                .append("<th>Empresa</th>")
+                .append("<th>Data Início</th>")
+                .append("<th>Data Fim</th>")
+                .append("</tr>");
+        for (Contrato c : contratos) {
+            Date inicio = c.getDtInicio();
+            Date fim = c.getDtFim();
+
+            html.append("<tr>")
+                    .append("<td>").append(c.getNumContrato()).append("</td>")
+                    .append("<td>").append(c.getIdContratante().getRazaoSocial()).append("</td>")
+                    .append("<td>").append(df.format(inicio)).append("</td>")
+                    .append("<td>").append(df.format(fim)).append("</td>")
+                    .append("</tr>");
+        }
+        html.append("</table>");
+        html.append("</body></html>");
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setTo("luiz.jacson@souunit.com.br");
+        helper.setSubject("Aviso: Contratos próximos do vencimento");
+        helper.setText(html.toString(), true);
+
+        mailSender.send(message);
     }
 }
