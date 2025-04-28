bfa3026ff486395e458e79ba3f31db65c5938989
Gabriel Ramos <134166074+gabrieIramos@users.noreply.github.com>
2025-04-28 16:43:48 -0300

    feat: email dos entregaveis expirando

--- src/main/java/com/getinfo/gestaocontratual/repository/EntregaveisRepository.java
+++ src/main/java/com/getinfo/gestaocontratual/repository/EntregaveisRepository.java
@@ -1,8 +1,10 @@
 package com.getinfo.gestaocontratual.repository;
+import com.getinfo.gestaocontratual.entities.Contrato;
 import com.getinfo.gestaocontratual.entities.Entregaveis;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,4 +12,5 @@ public interface EntregaveisRepository extends JpaRepository<Entregaveis, Long> 
     Optional<Entregaveis> findById (Long idEntregavel);
     List<Entregaveis> findByIdContrato_IdContrato(Long idContrato);
     void deleteAllByIdContrato_IdContrato(Long idContrato);
+    List<Entregaveis> findByDtFimBetween(LocalDate dtInicio, LocalDate dtFinal);
 }
