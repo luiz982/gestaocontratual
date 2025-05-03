c9ee42219724f0a0a1f9504df7f686f28828b0b5
luiz982 <juninholjl@hotmail.com>
2025-05-03 10:48:53 -0300

    feat: alteração de documentos base64 -> supabase storage

--- src/main/java/com/getinfo/gestaocontratual/controller/DocumentoController.java
+++ src/main/java/com/getinfo/gestaocontratual/controller/DocumentoController.java
@@ -1,22 +1,25 @@
 package com.getinfo.gestaocontratual.controller;
 
-import com.getinfo.gestaocontratual.controller.dto.CreateDocumentoRequest;
-import com.getinfo.gestaocontratual.controller.dto.DocumentoResponse;
 import com.getinfo.gestaocontratual.entities.Contrato;
 import com.getinfo.gestaocontratual.entities.Documentos;
 import com.getinfo.gestaocontratual.repository.ContratoRepository;
 import com.getinfo.gestaocontratual.repository.DocumentoRepository;
-import com.getinfo.gestaocontratual.utils.Validadores;
+import com.getinfo.gestaocontratual.service.DocumentoService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import com.getinfo.gestaocontratual.controller.dto.AlteraDocumentoRequest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/documentos")
@@ -26,78 +29,94 @@ public class DocumentoController {
     private DocumentoRepository documentoRepository;
     @Autowired
     private ContratoRepository contratoRepository;
+    @Autowired
+    private DocumentoService documentoService;
 
     @Operation(summary = "Cadastro de documentos")
     @Transactional
     @PostMapping("/criarDocumento")
-    public ResponseEntity<?> criarDocumento(@RequestBody CreateDocumentoRequest request) {
-        int MAX_FILE_SIZE = 5 * 1024 * 1024;
-        int MAX_BASE64_LENGTH = (int) Math.ceil(MAX_FILE_SIZE * 1.37);
-
-        byte[] conteudoDecodificado;
-
-
-        if (request.conteudoBase64() == null || request.conteudoBase64().isBlank()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Erro: O campo de conteúdo do documento está vazio ou nulo.");
-        }
-
-        if (request.conteudoBase64().length() > MAX_BASE64_LENGTH) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Erro: O documento excede o limite de 5MB.");
-        }
-
-        if (!Validadores.isBase64Valido(request.conteudoBase64())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Erro: Tipo de documento não permitido.");
-        }
-
-        try {
-            conteudoDecodificado = Base64.getDecoder().decode(request.conteudoBase64());
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Erro: O documento enviado não é um Base64 válido.");
-        }
-
-        Optional<Contrato> contratoOptional = contratoRepository.findById(request.idContrato());
+    public ResponseEntity<?> criarDocumento(@RequestPart("documento") MultipartFile file, @RequestParam("nome") String nome, @RequestParam("idContrato") Long idContrato) {
+        Optional<Contrato> contratoOptional = contratoRepository.findById(idContrato);
 
         if (contratoOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("Erro: Contrato não encontrado com o ID informado.");
         }
 
+        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+        try {
+            documentoService.uploadFile(fileName, file);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao fazer upload do arquivo para o Supabase Storage: " + e.getMessage());
+        }
+
+        String fileUrl = "https://dczwhcefblmnjnhhpvzn.supabase.co/storage/v1/object/public/" + "documentos/" + fileName;
+
         Documentos documento = new Documentos();
         documento.setContrato(contratoOptional.orElse(null));
-        documento.setNome(request.nome());
-        documento.setConteudo(conteudoDecodificado);
-
+        documento.setNome(nome);
+        documento.setUrl(fileUrl);
         Documentos salvo = documentoRepository.save(documento);
         return ResponseEntity.ok(salvo);
     }
-    @Operation(summary = "Deletar documento pelo id")
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deletarDocumento(@PathVariable Long id) {
-        Optional<Documentos> doc = documentoRepository.findById(id);
 
-        if (doc.isEmpty()) {
+    @Operation(summary = "Alterar documento")
+    @Transactional
+    @PutMapping("/{id}")
+    public ResponseEntity<?> alterarDocumento(@PathVariable Long id, @RequestPart(value = "documento", required = false) MultipartFile file, @RequestPart("nome") String nome) {
+        Optional<Documentos> documentoOptional = documentoRepository.findById(id);
+
+        if (documentoOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("Erro: Documento não encontrado com o ID informado.");
         }
 
+        Documentos documento = documentoOptional.get();
+        String fileUrl = documento.getUrl();
+
+        if (file != null && !file.isEmpty()) {
+            String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+            try {
+                documentoService.uploadFile(fileName, file);
+
+                documentoService.deleteFile(fileUrl.substring(fileUrl.lastIndexOf('/') + 1));
+
+                fileUrl = "https://dczwhcefblmnjnhhpvzn.supabase.co/storage/v1/object/public/" + "documentos/" + fileName;
+            } catch (IOException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Erro ao fazer upload do arquivo para o Supabase Storage: " + e.getMessage());
+            }
+        }
+
+        documento.setNome(nome);
+        documento.setUrl(fileUrl);
+        Documentos salvo = documentoRepository.save(documento);
+        return ResponseEntity.ok(salvo);
+    }
+
+    @Operation(summary = "Deletar documento pelo id")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarDocumento(@PathVariable Long id) {
+        Optional<Documentos> docOptional = documentoRepository.findById(id);
+
+        if (docOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Erro: Documento não encontrado com o ID informado.");
+        }
+        Documentos doc = docOptional.get();
+        documentoService.deleteFile(doc.getUrl().substring(doc.getUrl().lastIndexOf('/') + 1));
         documentoRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @Operation(summary = "Listar todos os documentos de um contrato")
     @GetMapping("/{idContrato}")
-    public ResponseEntity<List<DocumentoResponse>> listarDocumentosPorContrato(@PathVariable Long idContrato) {
-        List<DocumentoResponse> response = documentoRepository.findByContratoId(idContrato).stream()
-                .map(doc -> new DocumentoResponse(
-                        doc.getNome(),
-                        Base64.getEncoder().encodeToString(doc.getConteudo())
-                ))
-                .toList();
-
-        return ResponseEntity.ok(response);
+    public ResponseEntity<List<Documentos>> listarDocumentosPorContrato(@PathVariable Long idContrato) {
+        List<Documentos> documentos = documentoRepository.findByContratoId(idContrato);
+        return ResponseEntity.ok(documentos);
     }
 }
+
